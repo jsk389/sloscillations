a42a0e5dc978c487f4db6b22bda13b256ff7f01f
jsk389 <kuszi@DESKTOP-R7H6Q2N.localdomain>
2020-04-10 19:31:53 +0100

    Cleaning up code and adding comments

--- src/gp_lorenztian.py
+++ src/gp_lorenztian.py
@@ -35,14 +35,18 @@ class AsteroTerm(terms.SHOTerm):
     def get_all_coefficients(self, params):
 
         # Convert to cycles per day
+        # Frequency was in units of uHz now cpd
         nu0_cpd = params[1] * (86400.0 / 1e6)
+        # Linewidth was in units of uHz now cpd
         lwd_cpd = params[2] * (86400.0 / 1e6)
+        # Height was in units of ppm^2/uHz now in ppm^2/cpd
         height_cpd = params[0] * (1e6/86400.0)
 
         # Account for normalisation in height
         height = height_cpd * np.sqrt(np.pi/2) / 4
-        # 
+        # Convert frequency from cpd to radians
         omega0 = 2 * np.pi * nu0_cpd 
+        # Compute the oscillation quality factor
         Q = nu0_cpd / lwd_cpd
 
         #
@@ -50,7 +54,7 @@ class AsteroTerm(terms.SHOTerm):
         for i in range(1):
             log_S0 = np.log(height) - 2*np.log(Q)
             log_Q = np.log(Q)
-            log_omega0 = np.log(omega0)
+            log_omega0 = np.log(omega0) # - (i*2*np.pi)*86400.0/1e6) Don't forget omega is in units of rads/day
             coeffs.append(super(AsteroTerm, self).get_all_coefficients([log_S0, log_Q, log_omega0]))
         return [np.concatenate(args) for args in zip(*coeffs)]
 
@@ -81,7 +85,8 @@ def compute_mode(params, white=0):
     kernel = AsteroTerm(height=params[0],
                         freq = params[1],
                         lwd = params[2])
-    kernel += terms.JitterTerm(log_sigma = np.log(white))
+    if white > 0:
+        kernel += terms.JitterTerm(log_sigma = np.log(white))
 
     gp = celerite.GP(kernel)
     return kernel, gp, S0
@@ -117,54 +122,75 @@ def compute_full_model(f, params, dt, white=0):
 
 if __name__=="__main__":
 
+    # Cadence in seconds
     dt = 29.4*60
+    # Time array - 1000 days
     t = np.arange(0, 1000*86400, dt)
+    # Compute Nyquist frequency
     nyq = 1.0 / (2*dt)
+    # Compute bin width
     bw = 1.0 / t[-1]
+    # Convert time array into days from seconds
     t = (t/86400.0) * u.day
   
+    # Set up oscillation test parameters
     params = [10.0, 100.0, 0.2]
+    # White noise level
     white = 1.0
+    # Compute the kernel of the oscillation mode
     kernel, gp, S0 = compute_mode(params, white=white)
+    # Compute
     gp.compute(t.value)
     # model from Kallinger (2014)
+    # Sample from gp 
     y = gp.sample()
 
+    # Plot time-series
     plt.plot(t/86400.0, y)
     plt.show()
 
+    # Give units of ppm for lightkurve
     y = y*cds.ppm
-    print(y)
+
+    #
     lc = lk.LightCurve(time=t, flux=y)
 
     # Approximate Nyquist Frequency and frequency bin width in terms of days
     nyquist = 0.5 * (1./(np.median(np.diff(lc.time))))
 
+    # Compute periodogram
     ps = lc.to_periodogram(normalization='psd',
                            freq_unit=u.microhertz)
 
+    # Compute frequency array for analytical mode profile computation
+    # and for evaluating gp psd
     f = np.arange(bw, nyq, bw) * 1e6
+    # Standard lorentzian profile
     lor = compute_lor_model(f, params, dt, white=white)
+    # Analytical psd for chosen gp kernel
     full = compute_full_model(f, params, dt, white=white)
 
-    #backg_model = compute_backg_model(f*1e-6, S0, freqs*1e-6)
-#    backg_model = gran_backg(f, amps, freqs)
     # Convert frequency to 1/day for computation of psd
-    psd = kernel.get_psd(2*np.pi*f*(86400.0/1e6))# / (2 * np.pi)
-    print(t.max())
-    # Get back into correct units
+    psd = kernel.get_psd(2*np.pi*f*(86400.0/1e6))
+
+    # Get back into correct units i.e. normalisation
     psd *= (2 / (t.max().value))
     psd *= (2 / (f[1]-f[0]))
 
-    #plt.plot(ps.frequency, ps.power)
+    # Plot PSD and analytical models
+    plt.plot(ps.frequency, ps.power)
+
     # Have to add in white noise component to psd as celerite doesn't do
     # it by default 
     plt.plot(f, psd + 2e-6*white**2*dt, color='r')
+
     print(np.sum(lor)/np.sum(psd + 2e-6*white**2*dt))
     plt.plot(f, lor, color='k')
     plt.plot(f, full, color='g')
     #plt.plot(f*1e6, backg_model)
+    plt.xlim(99, 101)
     plt.show()
 
-    plt.plot(f, (lor) / full)
+    #plt.plot(f, (lor) / full)
+    plt.plot(f, ps.power / lor)
     plt.show()
