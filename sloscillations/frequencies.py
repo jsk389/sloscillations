8ddc2686f476ee13073328df98be5727e73a4a55
jsk389 <kuszi@DESKTOP-R7H6Q2N.localdomain>
2020-04-13 20:39:45 +0100

    Adding rotational splitting calculations

--- sloscillations/frequencies.py
+++ sloscillations/frequencies.py
@@ -4,14 +4,15 @@ import mixed_modes_utils as mixed_modes
 import numpy as np 
 import scaling_relations as scalings
 
+import matplotlib.pyplot as plt
 
 class Frequencies:
 
     def __init__(self, frequency, numax, delta_nu, radial_order_range=[-5, 5]):
 
+        self.frequency = frequency
         self.numax = numax
         self.delta_nu = delta_nu
-        self.frequency = frequency
         self.radial_order_range = radial_order_range
 
         # Epsilon 
@@ -27,31 +28,184 @@ class Frequencies:
         
         # Small separations
         self.d02 = scalings.d02_scaling(self.n, self.delta_nu)
-        self.d01 = scalings.d02_scaling(self.n)
+        self.d01 = scalings.d01_scaling(self.n)
 
-    def asymptotic_expression(self, l, d0l=0):
+    def asymptotic_expression(self, l, d0l=0, n=None):
         """
-        Generate mode frequencies according to the asymptotic expression
+        Generate mode frequencies according to the asymptotic expression.
+        Added n as an optional input because have to generate nominal p-modes
+        one radial order above and below radial modes to generate mixed modes
+        with full coverage.
         """
-        n_vals = self.n - self.n_max
-        return (self.n + self.epsilon_p + l/2, self.alpha/2*n_vals**2) * self.delta_nu \
-                - d0l, np.array([l]*len(self.n))
+        if n is None:
+            n_vals = self.n - self.n_max
+            return (self.n + self.epsilon_p + l/2 + self.alpha/2*n_vals**2) * self.delta_nu \
+                    - d0l, np.array([l]*len(self.n))
+        else:
+            n_vals = n - self.n_max
+            return (n + self.epsilon_p + l/2 + self.alpha/2*n_vals**2) * self.delta_nu \
+                    - d0l, np.array([l]*len(n))
 
     def generate_radial_modes(self):
+        """
+        Generate l=0 mode frequencies
+        """
         self.l0_freqs, self.l0_l = self.asymptotic_expression(l=0)
         
     def generate_quadrupole_modes(self):
+        """
+        Generate l=2 mode frequencies
+        """
         self.l2_freqs, self.l2_l = self.asymptotic_expression(l=2, d0l=self.d02)
 
     def generate_nominal_dipole_modes(self):
-        self.l1_nom_freqs, self.l1_l = self.asymptotic_expression(l=1, d0l=self.d01)
+        """
+        Generate l=1 nominal p-mode frequencies
+        """
+        n = np.arange(self.n_max + self.radial_order_range[0] - 1,
+                                self.n_max + self.radial_order_range[1]+2,
+                                1)
+        self.l1_nom_freqs, self.l1_l = self.asymptotic_expression(l=1, d0l=np.append(self.d01, self.d01[:2]), n=n)
 
-    def generate_mixed_modes(self, DPi1, coupling, eps_g):
-        self.l1_mixed_freqs, self.l1_ml = mixed_modes.all_mixed_l1_freqs(self.delta_nu,
-                                                                         self.l1_nom_freqs,
-                                                                         DPi1,
-                                                                         eps_g,
-                                                                         coupling)    
+    def generate_mixed_dipole_modes(self, DPi1, coupling, eps_g):
+        """
+        Generate l=1 mixed mode frequencies
+        """
+        self.l1_mixed_freqs, self.l1_zeta = mixed_modes.all_mixed_l1_freqs(self.delta_nu,
+                                                                           self.l1_nom_freqs,
+                                                                           DPi1,
+                                                                           eps_g,
+                                                                           coupling,
+                                                                           calc_zeta=True)                                                        
+        cond = (self.l1_mixed_freqs > self.l1_nom_freqs.min()) & (self.l1_mixed_freqs < self.l1_nom_freqs.max())
+        self.l1_mixed_freqs = self.l1_mixed_freqs[cond]
+        self.l1_zeta = self.l1_zeta[cond]
+        # Still need to assign radial  order to mixed modes!
+
+    def generate_rotational_splittings(self, split_core, DPi1, coupling, eps_g, split_env=0., l=1, method='simple'):
+        """
+        Generate rotational splittings
+        """
+        if l == 1:
+            if method == 'simple':
+                # Use method from Dehevuels et al. (2015)
+                splitting = (split_core/2 - split_env) * self.l1_zeta + split_env
+                self.l1_mixed_freqs_p1 = self.l1_mixed_freqs + splitting
+                self.l1_mixed_freqs_n1 = self.l1_mixed_freqs - splitting
+            elif method == 'Mosser':
+                # User method from Mosser et al. (2019)
+                # Stretch pds for given DPi1, coupling and eps_g values
+                zeta_func = mixed_modes.zeta_interp(self.frequency,
+                                                          self.l1_nom_freqs, 
+                                                          self.delta_nu, 
+                                                          DPi1, 
+                                                          coupling, 
+                                                          eps_g)
+                self.l1_mixed_freqs_p1, self.l1_mixed_freqs_n1, \
+                    self.l1_int_zeta_p, self.l1_int_zeta_n = mixed_modes.l1_theoretical_rot_M(self.l1_mixed_freqs,
+                                                                                              split_core, 
+                                                                                              zeta_func)
+            else:
+                sys.exit("Oh dear, method keyword isn't correct!")
+        else:
+            pass
+
+    def plot_echelle(self, l0=True, l2=True, l1=True, mixed=True, rotation=True):
+        """
+        Plot an echelle of frequencies
+        """
+        if l0:
+            plt.plot(self.l0_freqs % self.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                    self.l0_freqs,
+                    color='r', marker='D', label='$\ell=0$', linestyle='None', zorder=1, markersize=5)
+        if l2:
+            plt.plot(self.l2_freqs % self.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                    self.l2_freqs, 
+                    color='g', marker='s', label='$\ell=2$', linestyle='None', zorder=1, markersize=5)
+        if l1:
+            plt.plot(self.l1_nom_freqs % frequencies.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                    self.l1_nom_freqs, 
+                    color='b', marker='o', label='Nominal $\ell=1$', linestyle='None', zorder=1, markersize=5)
+        if mixed:
+            for i in range(len(self.l1_mixed_freqs)):
+                color = next(plt.gca()._get_lines.prop_cycler)['color']
+                plt.plot(self.l1_mixed_freqs[i] % self.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                        self.l1_mixed_freqs[i], 
+                        color=color, marker='v', label='Mixed $\ell=1$, $m=0$', linestyle='None', zorder=0, markersize=3)
+                if rotation:
+                    plt.plot(self.l1_mixed_freqs_p1[i] % self.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                            self.l1_mixed_freqs_p1[i],
+                            color=color, marker='<', label='Mixed $\ell=1$, $m=+1$', linestyle='None', zorder=0, markersize=3)
+                    plt.plot(self.l1_mixed_freqs_n1[i] % self.delta_nu - self.epsilon_p + 0.1*self.delta_nu, 
+                            self.l1_mixed_freqs_n1[i], 
+                            color=color, marker='>', label='Mixed $\ell=1$, $m=-1$', linestyle='None', zorder=0, markersize=3)
+        plt.xlim(0, self.delta_nu)
+        plt.xlabel(r'$\nu$ mod $\Delta\nu$ ($\mu$Hz)', fontsize=18)
+        plt.ylabel(r'Frequency ($\mu$Hz)', fontsize=18)
+        #plt.legend(loc='best', title='Mode degree')
+        plt.show()
 
 if __name__=="__main__":
-    pass
+    
+
+    frequency = np.arange(0.00787, 283., 0.00787)
+
+    # Set up frequencies class
+    frequencies = Frequencies(frequency=frequency,
+                              numax=103.2, 
+                              delta_nu=9.57, 
+                              radial_order_range=[-5, 5])
+
+    # l=0 modes
+    frequencies.generate_radial_modes()
+
+    # l=2 modes
+    frequencies.generate_quadrupole_modes()
+
+    # l=1 nominal p-modes
+    frequencies.generate_nominal_dipole_modes()
+
+    # l=1 mixed modes
+    frequencies.generate_mixed_dipole_modes(DPi1=77.9, 
+                                            coupling=0.2, 
+                                            eps_g=0.0)
+
+    # l=1 rotation
+    frequencies.generate_rotational_splittings(split_core=0.5, 
+                                               DPi1=77.9, 
+                                               coupling=0.2, 
+                                               eps_g=0., 
+                                               split_env=0., 
+                                               l=1, method='Mosser')
+
+    # Plot and echelle to check everything makes sense
+    frequencies.plot_echelle()
+
+
+    #plt.plot(frequencies.l0_freqs % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #         frequencies.l0_freqs,
+    #         color='r', marker='D', label='$\ell=0$', linestyle='None', zorder=1, markersize=5)
+    #plt.plot(frequencies.l2_freqs % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #         frequencies.l2_freqs, 
+    #         color='g', marker='s', label='$\ell=2$', linestyle='None', zorder=1, markersize=5)
+    #plt.plot(frequencies.l1_nom_freqs % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #         frequencies.l1_nom_freqs, 
+    #         color='b', marker='o', label='Nominal $\ell=1$', linestyle='None', zorder=1, markersize=5)
+    #for i in range(len(frequencies.l1_mixed_freqs)):
+    #    color = next(plt.gca()._get_lines.prop_cycler)['color']
+    #    plt.plot(frequencies.l1_mixed_freqs[i] % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #             frequencies.l1_mixed_freqs[i], 
+    #             color=color, marker='v', label='Mixed $\ell=1$, $m=0$', linestyle='None', zorder=0, markersize=3)
+    #    plt.plot(frequencies.l1_mixed_freqs_p1[i] % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #             frequencies.l1_mixed_freqs_p1[i],
+    #             color=color, marker='<', label='Mixed $\ell=1$, $m=+1$', linestyle='None', zorder=0, markersize=3)
+    #    plt.plot(frequencies.l1_mixed_freqs_n1[i] % frequencies.delta_nu - frequencies.epsilon_p + 0.1*frequencies.delta_nu, 
+    #             frequencies.l1_mixed_freqs_n1[i], 
+    #             color=color, marker='>', label='Mixed $\ell=1$, $m=-1$', linestyle='None', zorder=0, markersize=3)
+    #plt.xlim(0, frequencies.delta_nu)
+    #plt.xlabel(r'$\nu$ mod $\Delta\nu$ ($\mu$Hz)', fontsize=18)
+    #plt.ylabel(r'Frequency ($\mu$Hz)', fontsize=18)
+    ##plt.legend(loc='best', title='Mode degree')
+    #plt.show()
+
+    
--- sloscillations/mixed_modes_utils.py
+++ sloscillations/mixed_modes_utils.py
@@ -3,39 +3,41 @@
 import itertools
 import numpy as np 
 
+from scipy import interpolate
+from scipy.integrate import quad
 from scipy.optimize import brentq
 
-def all_mixed_l1_freqs(delta_nu, nu_p, DPi1, eps_g, coupling):
-    #l1_freqs = []
-    #for i in range(len(freq_zero)):
-    #    l1_freqs.append(find_mixed_l1_freq(freq, delta_nu, freq_zero[i], 
-    #                                                      period_spacing, 
-    #                                                      epsilon_g, coupling))
-    #return np.array(l1_freqs)
+def all_mixed_l1_freqs(delta_nu, nu_p, DPi1, eps_g, coupling, calc_zeta=True):
 
     l1_freqs = []
+    zeta = []
     for i in range(len(nu_p)):
         tmp = find_mixed_l1_freqs(delta_nu, nu_p[i], 
                                 DPi1, eps_g, coupling)
+        if calc_zeta == True:
+            tmp_zeta = calculate_zeta(tmp, nu_p[i], delta_nu, DPi1, coupling, eps_g)
+            zeta.append(tmp_zeta)
         l1_freqs.append(tmp)
 
-    return np.array(list(itertools.chain(*l1_freqs)))
+    if calc_zeta:
+        return np.array(list(itertools.chain(*l1_freqs))), np.array(list(itertools.chain(*zeta)))
+    else:
+        return np.array(list(itertools.chain(*l1_freqs)))
 
-def find_mixed_l1_freqs(nu_p, DPi1, eps_g, coupling):
+def find_mixed_l1_freqs(delta_nu, nu_p, DPi1, eps_g, coupling):
     """
     Find all mixed modes in a given radial order
     """
 
     nmin = np.ceil(1 / (DPi1*1e-6 * (nu_p + (delta_nu/2))) - (1/2) - eps_g)
     nmax = np.ceil(1 / (DPi1*1e-6 * (nu_p - (delta_nu/2))) - (1/2) - eps_g)
-    #nmin -= 10
     nmax += 2
-    #st.write(nmin, nmax)
+
     frequencies = []
     for i in np.arange(nmin, nmax, 1):
-        tmp = find_mixed_l1_freq(nu_p, DPi1, eps_g, coupling, i)
+        tmp = find_mixed_l1_freq(delta_nu, nu_p, DPi1, eps_g, coupling, i)
         frequencies = np.append(frequencies, tmp)
-#    print(frequencies)
+
     return np.sort(frequencies[np.isfinite(frequencies)])
 
 def find_mixed_l1_freq(delta_nu, pone, DPi1, eps_g, coupling, N):
@@ -51,25 +53,95 @@ def find_mixed_l1_freq(delta_nu, pone, DPi1, eps_g, coupling, N):
     lower_bound = 1 / (DPi1*1e-6 * (N + 1/2 + eps_g)) + np.finfo(float).eps * 1e4
     upper_bound = 1 / (DPi1*1e-6 * (N - 1 + 1/2 + eps_g)) - np.finfo(float).eps * 1e4
 
-
-    #nu = np.linspace(pone-0.5*DeltaNu, pone+0.5*DeltaNu, 10000)
-
-    #plt.plot(nu, opt_func(nu))
-    #plt.axvline(lower_bound)
-    #plt.axvline(upper_bound)
-    #plt.show()
-    #sys.exit()
-
-    #st.write(lower_bound, upper_bound, pone)
-    #sys.exit()
-    #print(lower_bound, upper_bound)
-    #x = np.linspace(lower_bound-10, upper_bound+10, 1000)
-    #plt.plot(x, opt_func(x))
-    #plt.show()
-    #sys.exit()
-
     try:
         return brentq(opt_func, lower_bound, upper_bound)
     except:
-        print("No mixed modes found!")
         return np.nan
+
+def l1_rot_from_zeta(nu_0, nu_m, drot, zeta_fun):
+    """
+    Find rotational splitting 
+    """
+    
+    # Upper and lower limits for integration
+    # Minimum value of nu_0 and nu_m or maximum value
+    lower_limit = nu_0 if nu_0 < nu_m else nu_m
+    upper_limit = nu_0 if nu_0 > nu_m else nu_m
+
+    # Integrate zeta over that range
+    res = quad(zeta_fun, lower_limit, upper_limit)
+    int_zeta = res[0] / (nu_m - nu_0)
+    return nu_0 + drot * int_zeta, int_zeta
+
+def l1_rot_from_zeta_iter(nu_0, nu_m, drot, zeta_fun, tol, max_iters=50, curr_iter=1):
+    # Compute rotational splitting iteratively
+    # If no rotational splitting then return nu_m, or nu_0?
+
+    #if drot == 0:
+    #    return nu_0, np.nan
+    if curr_iter >= max_iters:
+        print("Maximum number of iterations reached without convergence")
+        return nu_m, np.nan
+    
+    nu_m_new, int_zeta = l1_rot_from_zeta(nu_0, nu_m, drot, zeta_fun)
+
+    if abs(nu_m_new - nu_m) < tol:
+        return nu_m_new, int_zeta
+    else:
+        return l1_rot_from_zeta_iter(nu_0, nu_m_new, drot, zeta_fun,
+                                     tol, max_iters, curr_iter+1)
+
+def l1_theoretical_rot_M(l1_m0_freqs, drot, zeta_fun, max_iters=50, tol=1e-4):
+
+    l_mp1_freqs = []
+    int_zeta_p = []
+    l_mn1_freqs = []
+    int_zeta_n = []
+    for i in range(len(l1_m0_freqs)):
+        tmp_p1, tmp_iz_p = l1_rot_from_zeta_iter(l1_m0_freqs[i], l1_m0_freqs[i]+drot,
+                              drot, zeta_fun, tol, max_iters)
+        tmp_n1, tmp_iz_n = l1_rot_from_zeta_iter(l1_m0_freqs[i], l1_m0_freqs[i]-drot,
+                              drot, zeta_fun, tol, max_iters)
+        l_mp1_freqs = np.append(l_mp1_freqs, tmp_p1)
+        int_zeta_p = np.append(int_zeta_p, tmp_iz_p)
+        l_mn1_freqs = np.append(l_mn1_freqs, tmp_n1)
+        int_zeta_n = np.append(int_zeta_n, tmp_iz_n)
+
+        #st.write(l1_m0_freqs[i], tmp_p1, tmp_p1-l1_m0_freqs[i])
+        #st.write(l1_m0_freqs[i], tmp_n1, l1_m0_freqs[i]-tmp_n1)
+        #sys.exit()
+    return l_mp1_freqs, l_mn1_freqs, int_zeta_p, int_zeta_n
+
+def calculate_zeta(freq, nu_p, DeltaNu, DPi1, coupling, eps_g):
+    # Deheuvels et al. (2015) <http://dx.doi.org/10.1051/0004-6361/201526449>
+    a1 = np.cos(np.pi * ((1 / (freq * DPi1*1e-6)) - eps_g))**2
+    a2 = np.cos(np.pi * ((freq - nu_p) / DeltaNu))**2
+    a3 = (freq**2 * DPi1*1e-6) / (coupling * DeltaNu)
+    b = 1 + a1 * a3 / a2
+    return 1/b
+    
+def zeta_interp(freq, nu_p, delta_nu, 
+                DPi1, coupling, eps_g,
+                numDPi1=100, DPi1_range=[0.99, 1.01]):
+    # Interpolate zeta function
+    l1_freqs = []
+    zeta = []
+    DPi1_vals = np.linspace(DPi1_range[0]*DPi1, DPi1_range[1]*DPi1, numDPi1)
+
+    for i in range(len(DPi1_vals)):
+        #print(DPi1_vals[i])
+        tmp_l1_freqs, tmp_zeta = all_mixed_l1_freqs(delta_nu, nu_p, DPi1_vals[i], eps_g, coupling, calc_zeta=True)
+
+        l1_freqs = np.append(l1_freqs, tmp_l1_freqs)
+        zeta = np.append(zeta, tmp_zeta)
+
+    l1_freqs = l1_freqs.ravel()
+    zeta = zeta.ravel()
+
+    idx = np.argsort(l1_freqs)
+    l1_freqs = l1_freqs[idx]
+    zeta = zeta[idx]
+
+
+    zeta_fun = interpolate.interp1d(l1_freqs, zeta)
+    return zeta_fun
